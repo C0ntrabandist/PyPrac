5428e2b314e765d93b251d133d3d6cb2232ab394
Ilya Leuhin <edu.leuhin@gmail.com>
2024-10-03 09:25:46 +0300

    Completed HW for 30.09

--- 20240930/3/prog.py
+++ 20240930/3/prog.py
@@ -1,18 +1,19 @@
-'''
+
 def mult(matrix1, matrix2):
 
   n = len(matrix1)
-  res = [[0] * n for _ in range(n)]
+  res = [[0 for i in range(n)] for j in range(n)]
+  
   for i in range(n):
     for j in range(n):
+      res[i][j] = 0
       for k in range(n):
-        res[i][j] += matrix1[i][k] * matrix2[k][j]
+        res[i][j] = res[i][j] + matrix1[i][k] * matrix2[k][j]
+  print()
   return res
-'''
 
-def in_matrix(n):
+def in_matrix(n, matrix):
 
-  matrix = []
   for _ in range(n):
     row = list(eval(input()))
     matrix.append(row)
@@ -23,20 +24,23 @@ mtx2 = []
 res_m = []
 
 str1 = list(eval(input()))
-n = len(str1)
 mtx1.append(str1)
-mtx1.append(in_matrix(n-1)) 
-mtx2 = in_matrix(n)
+n = len(str1)
+mtx1 = in_matrix(n-1, mtx1)
+mtx2 = in_matrix(n, mtx2)
 
-for i in range(n):
-    for j in range(n):
-        sum = 0
-        for k in range(n):
-            sum += mtx1[i][k] * mtx2[k][j]
-        print(sum, end='')
-        if j != n - 1: print('', end=',')
-    print()
 '''
+print('*----*')
+print(f"Матрица 1: {mtx1}")
+print('*----*')
+print(f"Матрица 2: {mtx2}")
+print('*----*')
+#print(type(mtx1[0][0]))
+#print(type(mtx1[0][0]))
+#print('----')
+'''
+res_m = mult(mtx1, mtx2)
+
+
 for m in res_m:
   print(*m, sep=",")
-'''
